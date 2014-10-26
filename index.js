df23a0f1b35734cc6f6c14dd3a9ce2bea2999bdb
adamgibbons <adam.d.gibbons@gmail.com>
2014-10-26 02:43:54 -0700

    handles filepath, cb params

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ var CalEvent = require('./lib/cal-event.js');
 var fs = require('fs');
 var path = require('path');
 
-module.exports = function (options) {
+function _writeToFile(options, filepath, cb) {
   var options = this.options || {};
   var cal = new CalEvent(options);
   var data = cal.getEvent();
+  var dest = filepath ? path.join(filepath) : path.join(__dirname, 'test-event.ics');
 
-  fs.writeFile(path.join(__dirname, 'test-event.ics'), data, function(err, success) {
+  fs.writeFile(path.join(dest), data, function(err) {
+    if (cb) {
+      if (err) return cb(err);
+      return cb(null, dest);
+    }
+
     if (err) throw err;
-    console.log("Success!");
+    return dest;
   });
+}
+
+module.exports = {
+  write: _writeToFile
 }
