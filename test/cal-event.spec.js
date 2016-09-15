a46a794781cd51581026c7f8e62b8e1bedba5e42
quilicicf <cquilici@restlet.com>
2016-09-15 22:52:14 +0200

    :new: Enable creation of a calendar with multiple events

--- test/cal-event.spec.js
+++ test/cal-event.spec.js
@@ -95,4 +95,73 @@ describe('ics', function () {
     });
   });
 
+  describe('getCalendar()', function () {
+    it('creates a default event when no options passed', function () {
+      var defaultEvent = ics.getCalendar({});
+      var beginCalendarPosition = defaultEvent.search(new RegExp('BEGIN:VCALENDAR' + SYSTEM_LINE_BREAK));
+      var versionLinePosition = defaultEvent.search(new RegExp('VERSION:2.0' + SYSTEM_LINE_BREAK));
+      var beginEventPosition = defaultEvent.search(new RegExp('BEGIN:VEVENT' + SYSTEM_LINE_BREAK));
+      var descriptionPosition = defaultEvent.search(/DESCRIPTION/);
+      var summaryPosition = defaultEvent.search(new RegExp('SUMMARY:New Event' + SYSTEM_LINE_BREAK));
+      var endEventPosition = defaultEvent.search(new RegExp('END:VEVENT' + SYSTEM_LINE_BREAK));
+      var endCalendarPosition = defaultEvent.search(/END:VCALENDAR/);
+
+      var orderedPositions = [ beginCalendarPosition, versionLinePosition, beginEventPosition, descriptionPosition,
+        summaryPosition, endEventPosition, endCalendarPosition ];
+
+      expect(_.indexOf(orderedPositions, -1)).to.equal(-1);
+      expect(_.isEqual(_.sortBy(orderedPositions), orderedPositions)).to.equal(true);
+    });
+
+    it('has an event name', function () {
+      var calendarLines = ics.getCalendar({events: [ sampleEvent ]}).split(SYSTEM_LINE_BREAK);
+      expect(calendarLines.indexOf('SUMMARY:' + sampleEvent.eventName)).to.be.greaterThan(-1);
+    });
+
+    it('has multiple events', function () {
+      var calendarLines = ics.getCalendar({events: [ sampleEvent, sampleEvent ]}).split(SYSTEM_LINE_BREAK);
+      expect(_.countBy(calendarLines, function (value) {
+        return value === 'SUMMARY:' + sampleEvent.eventName;
+      }).true).to.equal(2);
+    });
+  });
+
+  describe('createCalendar()', function () {
+    it('creates event with every option passed', function () {
+      var expected = path.join(TMPDIR, 'calendar-event.ics');
+
+      ics.createCalendar({events: sampleEvent}, null, function (err, filepath) {
+        if (err) throw err;
+        expect(filepath).to.equal(expected);
+      });
+    });
+
+    it('returns a default filepath when no filename or filepath provided', function () {
+      var expected = path.join(TMPDIR, 'calendar-event.ics');
+
+      ics.createCalendar({}, null, function (err, filepath) {
+        if (err) throw err;
+        expect(filepath).to.equal(expected);
+      });
+    });
+
+    it('returns a default filepath and custom filename when filename provided', function () {
+      var expected = path.join(TMPDIR, 'custom-name.ics');
+
+      ics.createCalendar({filename: 'custom-name'}, null, function (err, filepath) {
+        if (err) throw err;
+        expect(filepath).to.equal(expected);
+      });
+    });
+
+    it('returns a custom filepath when one is provided', function () {
+      var expected = '/Users/gibber/Desktop/my-file.ics';
+
+      ics.createCalendar({}, '/Users/gibber/Desktop/my-file.ics', function (err, filepath) {
+        if (err) throw err;
+        expect(filepath).to.equal(expected);
+      });
+    });
+  });
+
 });
