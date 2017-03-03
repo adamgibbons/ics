52930c29e9f671088019ec70ca8260efbb40c9e3
Andrew Crowell <andrewj.crowell@gmail.com>
2017-03-02 16:04:07 -0800

    Add support for absolute (UTC) start time.

--- test/ics.spec.js
+++ test/ics.spec.js
@@ -100,6 +100,11 @@ describe('ICS', function() {
         .to.be.greaterThan(-1);
     });
 
+    it('sets an absolute, DATE-TIME-typed DTSTART when passed a DATE-TIME-typed start param with a UTC designator and no timezone', function() {
+      expect(ics.buildEvent({start: '2017-09-25T02:30:00.000Z'}).search('DTSTART:20170925T023000Z\r\n'))
+        .to.be.greaterThan(-1);
+    });
+
     it('sets a floating, DATE-TIME-typed DTEND with same value as DTSTART when passed a DATE-TIME-typed start param with neither a UTC designator nor a timezone', function() {
       expect(ics.buildEvent({start: '2017-09-25 02:30:00'}).search('DTEND:20170925T023000\r\n'))
         .to.be.greaterThan(-1);
