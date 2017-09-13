026b732cd5a305ece7a9a87ad481406e6ac33391
adamgibbons <adam.d.gibbons@gmail.com>
2017-09-12 21:09:26 -0600

    set alarm duration

--- src/utils/set-alarm.js
+++ src/utils/set-alarm.js
@@ -1,8 +1,41 @@
-export default function setAlarm({ name, email }) {
-  return null
-  // let formattedAttendee = 'CN='
-  // formattedAttendee += name || 'Unnamed attendee'
-  // formattedAttendee += email ? `:mailto:${email}` : ''
+function setDuration ({
+  weeks,
+  days,
+  hours,
+  minutes,
+  seconds
+}) {
+  let formattedString = 'P'
+  formattedString += weeks ? `${weeks}W` : ''
+  formattedString += days ? `${days}D` : ''
+  formattedString += 'T'
+  formattedString += hours ? `${hours}H` : ''
+  formattedString += minutes ? `${minutes}M` : ''
+  formattedString += seconds ? `${seconds}S` : ''
 
-  // return formattedAttendee
+  return formattedString
 }
+
+export default function setAlarm({
+  action,
+  repeat,
+  description,
+  duration
+}) {
+  let formattedString = 'BEGIN:VALARM\r\n'
+  formattedString += `ACTION:${action}\r\n`
+  formattedString += repeat ? `REPEAT:${repeat}\r\n` : ''
+  formattedString += description ? `DESCRIPTION:${description}\r\n` : ''
+  formattedString += duration ? `DURATION:${setDuration(duration)}\r\n` : ''
+  formattedString += 'END:VALARM\r\n'
+
+  return formattedString
+}
+
+// Example:  A duration of 15 days, 5 hours, and 20 seconds would be:
+
+// P15DT5H0M20S
+
+// A duration of 7 weeks would be:
+
+// P7W
--- test/utils/set-alarm.spec.js
+++ test/utils/set-alarm.spec.js
@@ -2,17 +2,46 @@ import { expect } from 'chai'
 import { setAlarm } from '../../src/utils'
 
 describe.only('utils.setAlarm', () => {
-  it('exists', () => {
-    expect(setAlarm).to.exist
+  it('sets an alarm', () => {
+    const attributes = {
+      repeat: 5,
+      description: 'Foo',
+      action: 'audio',
+      duration: {
+        weeks: 1,
+        days: 15,
+        hours: 3,
+        minutes: 4,
+        seconds: 50
+      }
+    }
+    const alarm = setAlarm(attributes)
+    expect(alarm).to.contain('BEGIN:VALARM')
+    expect(alarm).to.contain('ACTION:audio')
+    expect(alarm).to.contain('REPEAT:5')
+    expect(alarm).to.contain('DURATION:P1W15DT3H4M50S')
+    expect(alarm).to.contain('DESCRIPTION:Foo')
+    expect(alarm).to.contain('END:VALARM')
   })
 })
 
 
-       // BEGIN:VALARM
-       // TRIGGER;VALUE=DATE-TIME:19970317T133000Z
-       // REPEAT:4
-       // DURATION:PT15M
-       // ACTION:AUDIO
-       // ATTACH;FMTTYPE=audio/basic:ftp://example.com/pub/
-       //  sounds/bell-01.aud
-       // END:VALARM
+// BEGIN:VALARM
+// TRIGGER;VALUE=DATE-TIME:19970317T133000Z
+// REPEAT:4
+// DURATION:PT15M
+// ACTION:AUDIO
+// ATTACH;FMTTYPE=audio/basic:ftp://example.com/pub/
+//  sounds/bell-01.aud
+// END:VALARM
+
+// action: Joi.string().regex(/audio|display|email/).required(),
+// trigger: Joi.string().required(),
+// description: Joi.string(),
+// duration: Joi.string(),
+// repeat: Joi.string(),
+// attach: Joi.any(),
+// summary: Joi.string(),
+// attendee: contactSchema,
+// 'x-prop': Joi.any(),
+// 'iana-prop': Joi.any()
