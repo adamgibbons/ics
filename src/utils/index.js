a1c2c6bd0b4080f7f9492696713b2a49580cf3c3
adamgibbons <adam.d.gibbons@gmail.com>
2017-09-24 14:05:26 -0600

    remove legacy files

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,6 @@
 import setDate from './set-date'
 import setDateWithUTCtime from './set-date-with-utc-time'
 import setDateWithLocalTime from './set-date-with-local-time'
-import isValidStatus from './is-valid-status'
 import setGeolocation from './set-geolocation'
 import setContact from './set-contact'
 import setAlarm from './set-alarm'
@@ -10,7 +9,6 @@ export {
   setDate,
   setDateWithUTCtime,
   setDateWithLocalTime,
-  isValidStatus,
   setGeolocation,
   setContact,
   setAlarm
