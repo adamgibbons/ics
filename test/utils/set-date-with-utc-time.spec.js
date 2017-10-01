14119b96068e4e5dee69cff2736b547c4b226529
adamgibbons <adam.d.gibbons@gmail.com>
2017-10-01 16:26:14 -0600

    uncomment test

--- test/utils/set-date-with-utc-time.spec.js
+++ test/utils/set-date-with-utc-time.spec.js
@@ -11,27 +11,24 @@ describe.only('utils.setDateWithUTCtime', () => {
     const now = moment().utc().format('YYYYMMDDTHHmm00') + 'Z'
     expect(now).to.equal(setDateWithUTCtime())
   })
-  xit('sets a UTC date-time when passed an array of args', () => {
+  it('sets a UTC date-time when passed well-formed args', () => {
     const local = moment([2017, 9, 25, 0, 30])
-    const absolute = moment().utc([2017, 9, 25, 0, 30])
+    const absolute = moment([2017, 9, 25, 0, 30]).utc()
     const offset = local.utcOffset()
 
     if (offset === 0) {
-      expect(setDateWithUTCtime([2017, 9, 25, 0, 30]))
-        .to.equal(moment().utc().format('YYYYMMDDTHHmm00') + 'Z')
+      expect(setDateWithUTCtime([2017, 10, 25, 0, 30]))
+        .to.equal(absolute.format('YYYYMMDDTHHmm00') + 'Z')
     } else {
-
       let event
 
       if (Math.sign(offset) === -1) {
         event = moment(local).subtract(offset, 'minutes')
-        // console.log(absolute.subtract(offset, 'minutes'))
-        // console.log(event)
       } else {
         event = moment(local).add(offset, 'minutes')
       }
 
-      expect(setDateWithUTCtime([2017, 9, 25, 0, 30]))
+      expect(setDateWithUTCtime([2017, 10, 25, 0, 30]))
         .to.equal(event.format('YYYYMMDDTHHmm00') + 'Z')
     }
   })
