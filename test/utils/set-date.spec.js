8783308993e2db4adf99ce5e32bb6fe83c45d375
adamgibbons <adam.d.gibbons@gmail.com>
2017-10-01 17:03:23 -0600

    uncomment test

--- test/utils/set-date.spec.js
+++ test/utils/set-date.spec.js
@@ -3,7 +3,7 @@ import { setDate } from '../../src/utils'
 import { expect } from 'chai'
 
 describe('utils.setDate', () => {  
-  xit('defaults to UTC time when no type passed', () => {
+  it('defaults to UTC time when no type passed', () => {
     expect(setDate([2017, 7, 16, 22, 30])).to.equal('20170717T043000Z')
   })
   it('sets a local (i.e. floating) time when specified', () => {
