ed41c729a28d48b46908b7a1fb086a9680e3c819
sanjithkumar venkatesan <jeeth.tit.bit@gmail.com>
2017-12-28 15:09:05 +0530

    ICS with Multiple Events (#1)
    * ICS generation with multiple events
    
    * test cases updated
    
    * Test cases updated and Promise removed
    
    * format date test case reverted
    
    * console log removed
    
    * unwanted code removed

--- src/index.js
+++ src/index.js
@@ -2,35 +2,49 @@ import _ from 'lodash'
 import {
   buildEvent,
   validateEvent,
-  formatEvent
+  formatEvent,
+  formatCalendar
 } from './pipeline'
 
-export function createEvent (attributes, cb) {
-  if (!attributes) {
-    Error('attributes argument is required')
-  }
-
+export function generateEvent (attributes, cb) {
+  const { error, value } = validateEvent(buildEvent(attributes))
   if (!cb) {
     // No callback, so return error or value in an object
-    const { error, value } = validateEvent(buildEvent(attributes))
-
     if (error) return { error, value }
-
     let event = ''
-
     try {
       event = formatEvent(value)
     } catch(error) {
       return { error, value: null }
     }
-
     return { error: null, value: event }
   }
-
   // Return a node-style callback
-  const { error, value } = validateEvent(buildEvent(attributes))
-  
   if (error) return cb(error)
-
   return cb(null, formatEvent(value))
+}
+export function createEvent (data,productId, cb) {
+  let formatedEvents = ""
+  let events = []
+  if (!data || !productId) Error('attributes & productId is required')
+  if(_.isObject(data) && !_.isArray(data)){
+    events.push(data)
+  }else{
+    events = data
+  }
+  try {
+    _.forEach(events, (attributes)=> {
+      if (!attributes) throw Error('attributes argument is required')
+      generateEvent(attributes,(error,val)=>{
+        if(error) throw error
+          formatedEvents+=val
+      })
+    })
+    formatedEvents = formatCalendar(formatedEvents,productId)
+  } catch(error) {
+    if (!cb) return { error: error, value: null}
+    return cb(error, null)
+  }
+  if (!cb) return { error: null, value: formatedEvents}
+  return cb(null, formatedEvents)
 }
