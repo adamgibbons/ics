45960f035cbd56e9b077b3ccbf9fb05b18c56b9b
adamgibbons <adam.d.gibbons@gmail.com>
2018-02-08 22:43:55 -0700

    add createEvents function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash'
+import uuid from 'uuid/v4'
 import {
   buildEvent,
   validateEvent,
@@ -33,4 +34,27 @@ export function createEvent (attributes, cb) {
   if (error) return cb(error)
 
   return cb(null, formatEvent(value))
+}
+
+export function createEvents (events, cb) {
+  if (!events) {
+    return { error: Error('one argument is required'), value: null }
+  }
+
+  const result = events.map((event) => {
+    event.uid = event.uid || uuid()
+    return validateEvent(buildEvent(event))
+  }).map(({ error, value }) => {
+    if (error) {
+      return { error, value: null }
+    }
+
+    return { error: null, value: formatEvent(value) }
+  })
+
+  if (!cb) {
+    return result
+  }
+
+  return cb(null, result)
 }
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,30 +1,45 @@
 import { expect } from 'chai'
-import { createEvent } from '../src'
+import { createEvent, createEvents } from '../src'
 
 const invalidAttributes = { start: [] }
 const validAttributes = { start: [2000, 10, 5, 5, 0], duration: { hours: 1 } }
+const validAttributes2 = { start: [2001, 10, 5, 5, 0], duration: { hours: 1 } }
 
-describe('.createEvent', () => {
-  it('returns an error or value when not passed a callback', () => {
-    const event1 = createEvent(validAttributes)
-    const event2 = createEvent(invalidAttributes)
+describe('ics', () => {
+  describe('.createEvent', () => {
+    it('returns an error or value when not passed a callback', () => {
+      const event1 = createEvent(validAttributes)
+      const event2 = createEvent(invalidAttributes)
 
-    expect(event1.error).to.be.null
-    expect(event1.value).to.be.a('string')
-    expect(event2.error).to.exist
-  })
-  it('returns an error when passed an empty object', (done) => {
-    createEvent({}, (error, success) => {
-      done()
-      expect(error.name).to.equal('ValidationError')
-      expect(success).not.to.exist
+      expect(event1.error).to.be.null
+      expect(event1.value).to.be.a('string')
+      expect(event2.error).to.exist
+    })
+    it('returns an error when passed an empty object', (done) => {
+      createEvent({}, (error, success) => {
+        done()
+        expect(error.name).to.equal('ValidationError')
+        expect(success).not.to.exist
+      })
+    })
+    it('returns a node-style callback', (done) => {
+      createEvent(validAttributes, (error, success) => {
+        expect(error).not.to.exist
+        expect(success).to.contain('DTSTART:200010')
+        done()
+      })
     })
   })
-  it('returns a node-style callback', (done) => {
-    createEvent(validAttributes, (error, success) => {
-      expect(error).not.to.exist
-      expect(success).to.contain('DTSTART:200010')
-      done()
+  describe('.createEvents', () => {
+    it('returns an error when no arguments are passed', () => {
+      const events = createEvents()
+      expect(events.error).to.exist
+    })
+    it('returns a list of events', () => {
+      const [ first, second, third ] = createEvents([validAttributes, validAttributes2, invalidAttributes])
+      expect(first.value).to.contain('BEGIN:VCALENDAR')
+      expect(second.value).to.contain('BEGIN:VCALENDAR')
+      expect(third.error.name).to.equal('ValidationError')
     })
   })
 })
