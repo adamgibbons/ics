517ecf176d4b41db4b18316032e0a6bcf5c8331b
adamgibbons <adam.d.gibbons@gmail.com>
2018-02-09 19:57:37 -0700

    simplify funcs

--- src/index.js
+++ src/index.js
@@ -36,6 +36,46 @@ export function createEvent (attributes, cb) {
   return cb(null, formatEvent(value))
 }
 
+function format({ error, value }) {
+  if (error) {
+    return { error, value: null }
+  }
+
+  return { error: null, value: formatEvent(value) }
+}
+
+function bookend({ error, value }, idx, list) {
+
+  if (error) return { error, value }
+
+  if (idx === 0) {
+    // beginning of list
+    return { value: value.slice(0, value.indexOf('END:VCALENDAR')), error: null }
+  }
+
+  if (idx === list.length - 1) {
+    // end of list
+    return { value: value.slice(value.indexOf('BEGIN:VEVENT')), error: null}
+  }
+
+  return { error: null, value: value.slice(value.indexOf('BEGIN:VEVENT'), value.indexOf('END:VEVENT') + 12) }
+}
+
+function catenateEvents(accumulator, { error, value }, idx) {
+  if (error) {
+    accumulator.error = error
+    return accumulator
+  }
+
+  if (accumulator.value) {
+    accumulator.value = accumulator.value.concat(value)
+    return accumulator
+  }
+
+  accumulator.value = value
+  return accumulator
+}
+
 export function createEvents (events, cb) {
   if (!events) {
     return { error: Error('one argument is required'), value: null }
@@ -45,44 +85,7 @@ export function createEvents (events, cb) {
     event.uid = event.uid || uuid()
 
     return validateEvent(buildEvent(event))
-  }).map(({ error, value }) => {
-    if (error) {
-      return { error, value: null }
-    }
-
-    return { error: null, value: formatEvent(value) }
-  }).map(({ error, value }, idx, list) => {
-
-    if (error) return { error, value }
-
-    if (idx === 0) {
-      // beginning of list
-      return { value: value.slice(0, value.indexOf('END:VCALENDAR')), error: null }
-    }
-
-    if (idx === list.length - 1) {
-      // end of list
-      return { value: value.slice(value.indexOf('BEGIN:VEVENT')), error: null}
-    }
-
-    return { error: null, value: value.slice(value.indexOf('BEGIN:VEVENT'), value.indexOf('END:VEVENT') + 12) }
-
-  }).reduce((accumulator, { error, value }, idx) => {
-
-    if (error) {
-      accumulator.error = error
-      return accumulator
-    }
-
-    if (accumulator.value) {
-      accumulator.value = accumulator.value.concat(value)
-      return accumulator
-    }
-
-    accumulator.value = value
-    return accumulator
-
-  }, { error: null, value: null })
+  }).map(format).map(bookend).reduce(catenateEvents, { error: null, value: null })
 
   if (!cb) {
     return result
