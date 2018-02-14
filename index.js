9d6f3759e9f4ac3b03655c62a04fb889359fa2a2
ClueGuy <ClueGuy@mailinator.com>
2018-02-14 11:34:43 +0000

    remove what appears to be debug code

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-var ics = require('./dist');
+const ics = require('./dist');
 
 module.exports = ics;
-
-console.log(ics.createEvents([
-  {
-    title: 'Lunch',
-    start: [2018, 1, 15, 12, 15],
-    duration: { minutes: 45 }
-  },
-  {
-    title: 'Dinner',
-    start: [2018, 1, 15, 12, 15],
-    duration: { hours: 1, minutes: 30 }
-  }
-]))
