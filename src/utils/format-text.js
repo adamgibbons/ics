dc1520b59570997f5a5612eb5a660b6aaee80176
John French <john.french@pushgig.com>
2018-05-04 22:54:17 -0500

    fix order of text replacements so newlines don't get double escaped

--- src/utils/format-text.js
+++ src/utils/format-text.js
@@ -1,7 +1,7 @@
 export default function formatText (text) {
   return text
-    .replace(/\r?\n/gm, "\\n")
     .replace(/\\/gm, "\\\\")
+    .replace(/\r?\n/gm, "\\n")
     .replace(/;/gm, "\\;")
     .replace(/,/gm, "\\,")
 }
