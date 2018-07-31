7ee7d1053369b64951944af02b2734b8ae02c13f
Ferdinand Holzer <ferdinand.holzer@gmail.com>
2018-07-31 22:25:05 +0200

    Don't fold sequence line

--- src/pipeline/format.js
+++ src/pipeline/format.js
@@ -58,7 +58,7 @@ export default function formatEvent(attributes = {}) {
     }
   }
 
-  icsFormat += sequence ? (foldLine(`SEQUENCE:${sequence}`) + '\r\n') : ''
+  icsFormat += sequence ? (`SEQUENCE:${sequence}\r\n`) : ''
   icsFormat += description ? (foldLine(`DESCRIPTION:${setDescription(description)}`) + '\r\n') : ''
   icsFormat += url ? (foldLine(`URL:${url}`) + '\r\n') : ''
   icsFormat += geo ? (foldLine(`GEO:${setGeolocation(geo)}`) + '\r\n') : ''
