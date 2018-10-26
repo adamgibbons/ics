638728966e105014780a432fcba4eb2a11945c6d
Vincent Morvan <mvinc@vinc-2.local>
2018-10-26 10:48:34 +0200

    Add test for partstat in set-contact and fix setDateWithUTCtime test

--- test/utils/set-contact.spec.js
+++ test/utils/set-contact.spec.js
@@ -2,6 +2,29 @@ import { expect } from 'chai'
 import { setContact } from '../../src/utils'
 
 describe('utils.setContact', () => {
+  it('set a contact with partstat', () => {
+    const contact = { name: 'm-vinc', email: 'vinc@example.com' }
+    const contactUndefined = Object.assign({partstat: undefined}, contact)
+    const contactNeedsAction = Object.assign({partstat: 'NEEDS-ACTION'}, contact)
+    const contactAccepted = Object.assign({partstat: 'ACCEPTED'}, contact)
+    const contactDeclined = Object.assign({partstat: 'DECLINED'}, contact)
+    const contactTentative = Object.assign({contact, partstat: 'TENTATIVE'}, contact)
+
+    expect(setContact(contactUndefined))
+    .to.equal('RSVP=FALSE;CN=m-vinc:mailto:vinc@example.com')
+
+    expect(setContact(contactNeedsAction))
+    .to.equal('PARTSTAT=NEEDS-ACTION;RSVP=FALSE;CN=m-vinc:mailto:vinc@example.com')
+
+    expect(setContact(contactDeclined))
+    .to.equal('PARTSTAT=DECLINED;RSVP=FALSE;CN=m-vinc:mailto:vinc@example.com')
+
+    expect(setContact(contactTentative))
+    .to.equal('PARTSTAT=TENTATIVE;RSVP=FALSE;CN=m-vinc:mailto:vinc@example.com')
+    
+    expect(setContact(contactAccepted))
+    .to.equal('PARTSTAT=ACCEPTED;RSVP=FALSE;CN=m-vinc:mailto:vinc@example.com')
+  })
   it('sets a contact and defaults RSVP to false', () => {
     const contact1 = {
       name: 'Adam Gibbons',
