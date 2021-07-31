3b2f1f3e50a906eb7c89b8ca7ead7079740e76d4
lesion <lesion@autistici.org>
2021-07-31 18:08:07 +0200

    moving away from uuid and toward nanoid (#192)
    Co-authored-by: Adam Gibbons <adam.d.gibbons@gmail.com>

--- src/defaults.js
+++ src/defaults.js
@@ -1,11 +1,11 @@
-import uuidv1 from 'uuid/v1'
+import { nanoid } from 'nanoid'
 import { formatDate } from './utils'
 
 const defaults = {
   title: 'Untitled event',
   productId: 'adamgibbons/ics',
   method: 'PUBLISH',
-  uid: uuidv1(),
+  uid: nanoid(),
   timestamp: formatDate(null, 'utc'),
   start: formatDate(null, 'utc')
 }
