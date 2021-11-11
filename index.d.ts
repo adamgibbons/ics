016ee1d616c09c3ec49188a2245a8664e310e17c
Daniel Goboňa <daniel.gobona@cde.sk>
2021-11-11 18:53:04 +0100

    make htmlContent optional in typescript definition

--- index.d.ts
+++ index.d.ts
@@ -88,7 +88,7 @@ export type EventAttributes = {
   calName?: string;
   created?: DateArray;
   lastModified?: DateArray;
-  htmlContent: string;
+  htmlContent?: string;
 } & ({ end: DateArray } | { duration: DurationObject });
 
 export type ReturnObject = { error?: Error; value?: string };
