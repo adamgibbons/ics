b0a3dbbbffbd9fcd6400d90f083e44501a505725
Aske Hippe Brun <mail@askehippebrun.com>
2022-10-25 21:27:17 +0200

    Sequence zero bug (#225)
    * chore: upgrade package-lock.json
    
    * fix: sequence of value 0 is falsy bug
    
    * fix: remove undefined values instead of falsey values
    
    * test: add test for sequence 0

--- src/pipeline/format.js
+++ src/pipeline/format.js
@@ -68,7 +68,7 @@ export default function formatEvent(attributes = {}) {
     }
   }
 
-  icsFormat += sequence ? (`SEQUENCE:${sequence}\r\n`) : ''
+  icsFormat += typeof sequence !== 'undefined' ? (`SEQUENCE:${sequence}\r\n`) : ''
   icsFormat += description ? (foldLine(`DESCRIPTION:${setDescription(description)}`) + '\r\n') : ''
   icsFormat += url ? (foldLine(`URL:${url}`) + '\r\n') : ''
   icsFormat += geo ? (foldLine(`GEO:${setGeolocation(geo)}`) + '\r\n') : ''
