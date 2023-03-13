adde806701cc09adf86adbac48a51ded2257ec17
Adrian Curtin <48138055+AdrianCurtin@users.noreply.github.com>
2023-03-13 17:53:37 -0400

    Add additional fields to formatted attendee
    + format so that semicolon always ends first part and mailto always ends

--- src/utils/set-contact.js
+++ src/utils/set-contact.js
@@ -1,12 +1,30 @@
-export default function setContact({ name, email, rsvp, dir, partstat, role }) {
-  let formattedAttendee = ''
-  formattedAttendee += rsvp ? 'RSVP=TRUE;' : 'RSVP=FALSE;'
-  formattedAttendee += role ? `ROLE=${role};` : ''
-  formattedAttendee += partstat ? `PARTSTAT=${partstat};` : ''
-  formattedAttendee += dir ? `DIR=${dir};` : ''
-  formattedAttendee += 'CN='
-  formattedAttendee += name || 'Unnamed attendee'
-  formattedAttendee += email ? `:mailto:${email}` : ''
+export default function setContact({ name, email, rsvp, dir, partstat, role, cutype, xNumGuests }) {
+  let formattedParts = [];
+
+  if(cutype){
+    formattedParts.push("CUTYPE=".concat(cutype));
+  }
+  if(role){
+    formattedParts.push("ROLE=".concat(role));
+  }
+  if(partstat){
+    formattedParts.push("PARTSTAT=".concat(partstat));
+  }
+  if(rsvp){
+    formattedParts.push(rsvp ? 'RSVP=TRUE' : 'RSVP=FALSE');
+  }
+  if(dir){
+    formattedParts.push("DIR=".concat(dir));
+  }
+  
+  formattedParts.push('CN='.concat((name || 'Unnamed attendee')));
+
+  if(xNumGuests){
+    formattedParts.push('X-NUM-GUESTS='.concat(xNumGuests));
+  }
+
+  var formattedAttendee = formattedParts.join(';').concat(email ? ":mailto:".concat(email) : '');
+ 
 
   return formattedAttendee
 }
