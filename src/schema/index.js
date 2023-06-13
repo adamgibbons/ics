6d57f2b50168f651923d6a6cfb80d09e041adbf4
mmjr-x <2429824+mmjr-x@users.noreply.github.com>
2023-06-13 16:00:30 +0100

    Updated yup to ^1.2.0 and fixed breaking change (#243)
    Co-authored-by: Ernst <ernst.bank@talentmanager.nl>

--- src/schema/index.js
+++ src/schema/index.js
@@ -7,7 +7,7 @@ import * as yup from 'yup'
 const urlRegex = /^(?:([a-z0-9+.-]+):\/\/)(?:\S+(?::\S*)?@)?(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/
 
 const dateTimeSchema = yup.array().min(3).max(7).of(yup.lazy((item, options) => {
-    const itemIndex = parseInt(options.path.match(/.*\[(\d+)]/)[1])
+    const itemIndex = options.parent.indexOf(options.value)
     return [
       yup.number().integer(),
       yup.number().integer().min(1).max(12),
