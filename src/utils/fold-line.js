6e07e40632956ffb8228914ad4d0e6a7484ff6c5
Hayden Chen <hayden.chen.dev@gmail.com>
2023-12-14 08:37:06 +0800

    fix(utils): :bug: fixed the issue with foldLine not correctly handling strings containing emojis. (#258)
    closed #257
    
    Co-authored-by: Adam Gibbons <adam.d.gibbons@gmail.com>

--- src/utils/fold-line.js
+++ src/utils/fold-line.js
@@ -1,10 +1,12 @@
 
-export default function foldLine (line) {
+import runes from 'runes2'
+
+export default function foldLine(line) {
   const parts = []
   let length = 75
-  while (line.length > length) {
-    parts.push(line.slice(0, length))
-    line = line.slice(length)
+  while (runes(line).length > length) {
+    parts.push(runes.substring(line, 0, length))
+    line = runes.substring(line, length)
     length = 74
   }
   parts.push(line)
