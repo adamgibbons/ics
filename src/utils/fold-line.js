792b1cad2f57ea8b4a22c85520d345d4ed51cc25
Vladislav <24293461+kusyka911@users.noreply.github.com>
2024-06-11 18:01:12 +0300

    Fix runes error (#266)

--- src/utils/fold-line.js
+++ src/utils/fold-line.js
@@ -1,12 +1,12 @@
 
-import runes from 'runes2'
+import { runes, substring } from 'runes2'
 
 export default function foldLine(line) {
   const parts = []
   let length = 75
   while (runes(line).length > length) {
-    parts.push(runes.substring(line, 0, length))
-    line = runes.substring(line, length)
+    parts.push(substring(line, 0, length))
+    line = substring(line, length)
     length = 74
   }
   parts.push(line)
