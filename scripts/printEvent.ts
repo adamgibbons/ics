d25e1f0c4b2f422bd3f07ee25dae2ecd48ba0c1a
Adam Gibbons <adam.d.gibbons@gmail.com>
2026-03-31 22:16:02 -0600

    remove file

--- scripts/printEvent.ts
+++
@@ -1,8 +0,0 @@
-// import testEvent from "./fixtures.test.json";
-// import { IEventComponent } from "../src/components/event.component";
-// import { printVEvent } from "../src/index";
-
-// const result = printVEvent(testEvent as IEventComponent, {});
-
-// console.log(result);
-
